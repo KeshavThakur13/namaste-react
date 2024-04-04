38244c1bdf10fcf63017eda060dcd802054cc0e0
Keshav Kumar Thakur <keshav.thakur.58679@gmail.com>
2024-04-04 21:59:48 +0545

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+/*
+ <div id="parent">
+    <div id = "child1">
+        <h1>This is first</h1>
+        <h1>This is second</h1>
+    </div>
+    <div id = "child2">
+        <h1>This is first</h1>
+        <h1>This is second</h1>
+    </div>
+</div>
+*/
+// const heading = React.createElement('h1',{id:'heading'},'Hello World from Namaste React');
+const heading = React.createElement('div',{id:'parent'},
+[React.createElement('div',{id:'child1'},[React.createElement('h1',{},'This is first1'),React.createElement('h1',{},'This is second1')]),React.createElement('div',{id:'child2'},[React.createElement('h1',{},'This is first2'),React.createElement('h1',{},'This is second2')])]
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);

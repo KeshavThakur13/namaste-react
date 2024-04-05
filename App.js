0813fb52c1d9cb92bd6ca58a1f733fa3341c644a
Keshav Kumar Thakur <keshav.thakur.58679@gmail.com>
2024-04-05 10:39:28 +0545

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /*
  <div id="parent">
     <div id = "child1">
@@ -11,8 +13,8 @@
 </div>
 */
 // const heading = React.createElement('h1',{id:'heading'},'Hello World from Namaste React');
-const heading = React.createElement('div',{id:'parent'},
-[React.createElement('div',{id:'child1'},[React.createElement('h1',{},'This is first1'),React.createElement('h1',{},'This is second1')]),React.createElement('div',{id:'child2'},[React.createElement('h1',{},'This is first2'),React.createElement('h1',{},'This is second2')])]
+const heading = React.createElement('div', { id: 'parent' },
+    [React.createElement('div', { id: 'child1' }, [React.createElement('h1', {}, 'This is first1🚀🚀'), React.createElement('h1', {}, 'This is second1')]), React.createElement('div', { id: 'child2' }, [React.createElement('h1', {}, 'This is first2'), React.createElement('h1', {}, 'This is second2')])]
 )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
